8477f36f71561e360c24ff10d843dd793ec000cd
l0stman <endian.sign@gmail.com>
2010-03-31 20:30:42 +0300

    Make rsync to install files recursively.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ SITDIR=$INSTDIR/site
 SYSDIR=$INSTDIR/systems
 
 install -d $SITDIR $SYSDIR
-rsync -t * $SITDIR
+rsync -tr * $SITDIR
 cd $SYSDIR
 if ! [ -L blog.asd ]; then
     ln -s $SITDIR/blog.asd
