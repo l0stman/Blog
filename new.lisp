e0874c929e8e0490be8dc31d666a443da23a3109
l0stman <endian.sign@gmail.com>
2010-07-31 06:38:15 +0300

    Move the procedure del-conf into a new file delete.lisp.

--- new.lisp
+++ new.lisp
@@ -49,28 +49,6 @@
 		 (:input :type "submit"  :name "action" :value "view"))
 	   (str (fmt-help)))))
 
-(define-easy-handler (del-conf :uri "/delete"
-                               :default-request-type :post)
-    ((id :parameter-type 'integer) delete-p uri)
-  "Ask confirmation before deleting a post."
-  (w/auth
-   (cond ((not delete-p)
-          (w/html ()
-            (:form :method "post" :action "delete"
-                   (:div :class "message"
-                         (fmt "Do you really want to delete post ~D?" id))
-                   (:input :type "hidden" :name "id" :value id)
-                   (:input :type "hidden" :name "uri" :value uri)
-                   (:div :class "submit"
-                         (:input :type "submit" :name "delete-p" :value "yes")
-                         (:span :class "separator" " ")
-                         (:input :type "submit" :name "delete-p"
-                                 :value "no")))))
-         (t (when (string= delete-p "yes")
-              (delete-post id)
-              (save-blog))
-            (redirect uri)))))
-
 (define-easy-handler (new-post :uri "/new"
 			       :default-request-type :post)
     ((id :parameter-type 'integer)
