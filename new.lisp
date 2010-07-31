a00b833677b38c46c7e222fac9bbd5c8df50d922
l0stman <endian.sign@gmail.com>
2010-07-31 05:48:53 +0300

    Ask confirmation before deleting a blog post.

--- new.lisp
+++ new.lisp
@@ -49,6 +49,28 @@
 		 (:input :type "submit"  :name "action" :value "view"))
 	   (str (fmt-help)))))
 
+(define-easy-handler (del-conf :uri "/delete"
+                               :default-request-type :post)
+    ((id :parameter-type 'integer) delete-p uri)
+  "Ask confirmation before deleting a post."
+  (w/auth
+   (cond ((not delete-p)
+          (w/html ()
+            (:form :method "post" :action "delete"
+                   (:div :class "message"
+                         (fmt "Do you really want to delete post ~D?" id))
+                   (:input :type "hidden" :name "id" :value id)
+                   (:input :type "hidden" :name "uri" :value uri)
+                   (:div :class "submit"
+                         (:input :type "submit" :name "delete-p" :value "yes")
+                         (:span :class "separator" " ")
+                         (:input :type "submit" :name "delete-p"
+                                 :value "no")))))
+         (t (when (string= delete-p "yes")
+              (delete-post id)
+              (save-blog))
+            (redirect uri)))))
+
 (define-easy-handler (new-post :uri "/new"
 			       :default-request-type :post)
     ((id :parameter-type 'integer)
@@ -57,18 +79,15 @@
      action
      (uri :init-form (referer)))
   (w/auth
-      (cond ((string= action "delete")
-	     (delete-post id)
-	     (save-blog)
-	     (redirect uri))
-	    ((string= action "add") (add-post id title body uri))
-	    (t
-	     (cond ((string= action "view")
-		    (setf title (post-parameter "title")
-			  body (post-parameter "body")))
-		   (id
-		    (let ((p (find-post id)))
-		      (when p
-			(setf title (out-fmt (title p))
-			      body (out-fmt (body p))))))) 
-	     (new-form :id id :title title :body body :uri uri)))))
+   (cond ((string= action "delete") (del-conf :id id :uri uri))
+         ((string= action "add") (add-post id title body uri))
+         (t
+          (cond ((string= action "view")
+                 (setf title (post-parameter "title")
+                       body (post-parameter "body")))
+                (id
+                 (let ((p (find-post id)))
+                   (when p
+                     (setf title (out-fmt (title p))
+                           body (out-fmt (body p)))))))
+          (new-form :id id :title title :body body :uri uri)))))
