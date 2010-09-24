f82f2c5fc59730b72d3041e5b6670a410508254f
l0stman <endian.sign@gmail.com>
2010-09-24 07:11:10 +0300

    Define the unesc-html function and use it in out-fmt.  Remove completely the deffmt macro.

--- fmt.lisp
+++ fmt.lisp
@@ -1,27 +1,10 @@
 (in-package :blog)
 
-(defmacro deffmt (name (s &key start end) docstring &body plist)
-  "Define the function NAME that applies a regex based transformation
-to the string S. PLIST is a property list of regex and replacement
-string passed successively to `regex-replace-all'.  If supplied, the
-function START and the function END are applied respectively before
-and after the above transformations."
-  (flet ((call (fn) (if fn `((,s (,fn ,s)))))
-         (mklist (o) (if (listp o) o (list o))))
-    `(defun ,name (,s)
-       ,docstring
-       (let*
-	   (,@(call start)
-	    ,@(loop for (re repmt) on plist by #'cddr collect
-		   `(,s (regex-replace-all ,re ,s ,@(mklist repmt))))
-	      ,@(call end))
-	 s))))
-
 (defvar *ret* (coerce '(#\return #\newline #\return #\newline) 'string))
 
 (defun in-fmt (s)
     "Transform the ASCII string to HTML by escaping characters."
-    (escape-string s))
+    (esc-html s))
 
 (defun esc-html (src)
   "Escape all special HTML characters in the string SRC."
@@ -46,15 +29,50 @@ and after the above transformations."
               (otherwise (princ (aref src i) dst)))
             (incf i delta)))))
 
-(deffmt out-fmt (s)
-    "Transform back the HTML string to ASCII and unescape special characters."
-  "&lt;" "<"
-  "&gt;" ">"
-  "&quot;" "\""
-  "&(#x?[\\da-fA-F]+);"
-  (#'(lambda (m r1)
-       (declare (ignore m))
-       (let ((n (if (char= (elt r1 1) #\x) 0 1)))
-         (format nil "~a" (code-char (read-from-string (subseq r1 n))))))
-     :simple-calls t))
+(defmacro case-match ((src &key (start 0)) &body clauses)
+  (labels ((iter (clauses)
+             (when clauses
+               (let ((cl (car clauses)))
+                 (if (eq (car cl) t)
+                     `(progn ,@(cdr cl)) ; default statement
+                     `(multiple-value-bind (match-start match-end)
+                          (scan ,(car cl) ,src :start ,start)
+                        (if match-start
+                            (progn ,@(cdr cl))
+                            ,(iter (cdr clauses)))))))))
+    (iter clauses)))
+
+(defun unesc-html (src)
+  "Transform back all special HTML characters to ASCII in the string SRC."
+  (with-output-to-string (dst)
+    (loop
+       with i = 0
+       while (< i (length src))
+       do (if (char= (aref src i) #\&)
+              (case-match (src :start (1+ i))
+                ("^lt;"
+                 (princ #\< dst)
+                 (setq i match-end))
+                ("^gt;"
+                 (princ #\> dst)
+                 (setq i match-end))
+                ("^quot;"
+                 (princ #\" dst)
+                 (setq i match-end))
+                ("^amp;"
+                 (princ #\& dst)
+                 (setq i match-end))
+                ("^#039;"
+                 (princ #\' dst)
+                 (setq i match-end))
+                (t
+                 (princ #\& dst)
+                 (incf i)))
+              (progn
+               (princ (aref src i) dst)
+               (incf i))))))
+
+(defun out-fmt (s)
+  "Transform back the HTML string to ASCII and unescape special characters."
+  (unesc-html s))
 
