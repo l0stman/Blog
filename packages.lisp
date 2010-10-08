a882166444af449515fd91b792c89753ace48524
l0stman <endian.sign@gmail.com>
2010-10-08 18:57:49 +0300

    Shadow *session-max-time* which is also defined in hunchentoot.

--- packages.lisp
+++ packages.lisp
@@ -2,13 +2,13 @@
 
 (defpackage :blog
   (:use :hunchentoot :cl-who :cl :cl-ppcre)
-  (:shadow :defconstant)
+  (:shadow :defconstant
+           :*session-max-time*)
   (:import-from :ironclad
                 :make-hmac
                 :update-hmac
                 :hmac-digest
                 :derive-key
                 :make-kdf)
-  (:export
-   :save-blog
-   :load-blog))
+  (:export :save-blog
+           :load-blog))
